7ca764f79704cba4aa14a095007214630baaa683
JJ Resnick <jjresnick2020@gmail.com>
2025-02-18 01:36:44 -0800

    Adjust hero section spacing to bring text back up

--- src/pages/LandingPageB.tsx
+++ src/pages/LandingPageB.tsx
@@ -87,7 +87,7 @@ function LandingPageB() {
       </header>
 
       {/* Hero Section */}
-      <BackgroundBeamsWithCollision className="min-h-screen pt-[600px] bg-white relative z-[1]">
+      <BackgroundBeamsWithCollision className="min-h-screen pt-48 bg-white relative z-[1]">
         <div className="absolute inset-0 z-0">
           <img
             alt="Hero Background"
@@ -96,7 +96,7 @@ function LandingPageB() {
           />
         </div>
         <div className="container relative z-[2] mx-auto px-4 md:px-6">
-          <div className="flex flex-col items-center gap-12 mt-72">
+          <div className="flex flex-col items-center gap-12 mt-24">
             {/* Hero Text */}
             <div className="flex flex-col justify-center space-y-8 text-center max-w-5xl mx-auto">
               <div className="space-y-6">
