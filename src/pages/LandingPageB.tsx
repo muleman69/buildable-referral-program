a98ca9e516fc6f4625c62ecd4f4fb73cc5206c2d
JJ Resnick <jjresnick2020@gmail.com>
2025-02-18 01:26:12 -0800

    Further increase hero section spacing to prevent headline cutoff

--- src/pages/LandingPageB.tsx
+++ src/pages/LandingPageB.tsx
@@ -87,9 +87,9 @@ function LandingPageB() {
       </header>
 
       {/* Hero Section */}
-      <BackgroundBeamsWithCollision className="min-h-screen pt-96 bg-white relative z-[1]">
+      <BackgroundBeamsWithCollision className="min-h-screen pt-[600px] bg-white relative z-[1]">
         <div className="container relative z-[2] mx-auto px-4 md:px-6">
-          <div className="flex flex-col items-center gap-12 mt-48">
+          <div className="flex flex-col items-center gap-12 mt-72">
             {/* Hero Text */}
             <div className="flex flex-col justify-center space-y-8 text-center max-w-5xl mx-auto">
               <div className="space-y-6">
